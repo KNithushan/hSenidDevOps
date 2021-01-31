c9091ce2d6bfa38f1fe401d657ee6d7b21ec66ba
KNithushan <33048375+KNithushan@users.noreply.github.com>
2021-01-31 11:04:55 +0530

    Create CrossiteRequestorgery.js
    Cross-Site Request Forgery Attacks inclusion.

---
+++ CrossiteRequestorgery.js
@@ -0,0 +1,20 @@
+<script>
+    var csrf_token = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+
+    var app = angular.module("app", []);
+
+    app.config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.defaults.headers.post["anti-csrf-token"] = csrf_token;
+        $httpProvider.defaults.headers.put["anti-csrf-token"] = csrf_token;
+        $httpProvider.defaults.headers.patch["anti-csrf-token"] = csrf_token;
+        // AngularJS does not create an object for DELETE and TRACE methods by default, and has to be manually created.
+        $httpProvider.defaults.headers.delete = {
+            "Content-Type" : "application/json;charset=utf-8",
+            "anti-csrf-token" : csrf_token
+        };
+        $httpProvider.defaults.headers.trace = {
+            "Content-Type" : "application/json;charset=utf-8",
+            "anti-csrf-token" : csrf_token
+        };
+      }]);
+ </script>
